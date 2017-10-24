40f5004a63ca4eb7d332d41e3275aeecef723ffc
zhouzhengquan <zzhou@merkleinc.com>
2017-10-24 18:03:49 +0800

    Add files via upload

--- vue1.x/merkle.js
+++ vue1.x/merkle.js
@@ -6,20 +6,19 @@ window.digitalData.newEvent1 = function () {
 window.digitalData.eventHandler1 = function () {
     $(window).on('CustomTagEvent1', function () { //CustomTagEvent 加载完执行
         console.log('CustomTagEvent1');
-        _satellite.track('page_view');
+        _satellite.track('vuepageview1');
     });
 };
 window.digitalData.eventHandler1();
 
-window.digitalData = {};
 window.digitalData.newEvent2 = function () {
     $(window).trigger('CustomTagEvent1');    
 };
 
 window.digitalData.eventHandler2 = function () {
     $(window).on('CustomTagEvent2', function () { //CustomTagEvent 加载完执行
-        console.log('CustomTagEvent1');
-        _satellite.track('page_view');
+        console.log('CustomTagEvent2');
+        _satellite.track('vuepageview2');
     });
 };
 window.digitalData.eventHandler2();
