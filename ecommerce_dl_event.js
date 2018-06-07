06d5770e8b882ae459cfa5ee5f6d269116ad24ec
zhouzhengquan <zzhou@merkleinc.com>
2018-06-07 10:26:57 +0800

    Create ecommerce_dl_event.js

---
+++ ecommerce_dl_event.js
@@ -0,0 +1,23 @@
+ function ikea-pageflow-1(){
+    window.dataLayer.push({
+      'event': 'GA-event',
+      'eventDetail': { 
+        'category': 'pageflow 1', 
+        'action': 'pageflow 1',
+        'label': 'pageflow 1'
+      },
+      'ecommerce': undefined
+   });
+ }
+ 
+  function ikea-pageflow-2(){
+    window.dataLayer.push({
+      'event': 'GA-event',
+      'eventDetail': { 
+        'category': 'pageflow 2', 
+        'action': 'pageflow 2',
+        'label': 'pageflow 2'
+      },
+      'ecommerce': undefined
+   });
+ }
